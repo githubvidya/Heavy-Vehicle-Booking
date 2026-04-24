71ea6a9b2fa3fd426d2955c4053fd59306913803
Vidya Bhushan Tiwari <110774801+githubvidya@users.noreply.github.com>
2026-04-24 17:51:36 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://ornate-alfajores-57b8fd.netlify.app",
+      "https://heavy-vehicle.netlify.app",
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "DELETE", "PUT"],
